2a7f4da1c16b85fad84ab5fc4897e0f29d695b73
KanuriSathvika <117982917+KanuriSathvika@users.noreply.github.com>
2023-01-30 12:20:54 +0530

    Delete JavaScript directory

--- JavaScript/7/lastindex.js
+++
@@ -1,7 +0,0 @@
-/**07. Declare an array containing the multiple values and 
- * use lastIndexOf to determine the position of the first and last occurrence 
- * of a word "pw skills".
- */
-const skills=['ineuron','pw skills','javascript','pw skills'];
-console.log("First occurence of a word 'pw skills' =",skills.lastIndexOf('pw skills',2));
-console.log("Last occurence of a word 'pw skills' =",skills.lastIndexOf('pw skills'));
